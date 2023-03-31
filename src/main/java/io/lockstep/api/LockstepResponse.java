df559f5e3b8a8d8d356ee48a1abd386e37e4a998
Alex Russak <alex.russak@sage.com>
2023-03-31 16:39:20 -0700

    Fix javadoc imports

--- src/main/java/io/lockstep/api/LockstepResponse.java
+++ src/main/java/io/lockstep/api/LockstepResponse.java
@@ -87,7 +87,7 @@ public class LockstepResponse<T> {
      * Getter for the field <code>error</code>.
      * </p>
      *
-     * @return a {@link io.lockstep.api.models.ErrorResult} object.
+     * @return a {@link io.lockstep.api.ErrorResult} object.
      */
     public ErrorResult getError() {
         return this.error;
@@ -98,7 +98,7 @@ public class LockstepResponse<T> {
      * Setter for the field <code>error</code>.
      * </p>
      *
-     * @param error a {@link io.lockstep.api.models.ErrorResult} object.
+     * @param error a {@link io.lockstep.api.ErrorResult} object.
      */
     public void setError(ErrorResult error) {
         this.error = error;
